13bf09603a4b550add92434b69122c5f02fe3375
Abhishek Jaiswal <72942475+theabhishekjaiswal@users.noreply.github.com>
2025-05-25 18:43:11 +0530

    Create MergeSort.java

---
+++ MergeSort.java
@@ -0,0 +1,55 @@
+public class MergeSort{
+
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left >= right) return;
+
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+
+        // Merge both halves into temp[]
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        // Copy remaining elements of left half
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        // Copy remaining elements of right half
+        while (j <= right) {
+            temp[k++] = arr[j++];
+        }
+
+        // Copy temp[] back to original array
+        for (int m = 0; m < temp.length; m++) {
+            arr[left + m] = temp[m];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 8, 6, 2, 7, 4, 1};
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.print("Sorted array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
